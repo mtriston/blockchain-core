c5bab419b999f365fc5007092a6db88accd5caae
Alexander Spivak <spivak95alexander@gmail.com>
2021-09-17 16:27:22 +0300

    Init commit

---
+++ src/main/java/com/example/dchat/DchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.dchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dchat/DchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
